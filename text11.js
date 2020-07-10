1c1ecb2434ad3b5588d5de7eabeae29baad7a756
huweiwei <huweiwei@kedacom.com>
2020-07-10 20:33:37 +0800

    first commit

---
+++ text11.js
@@ -0,0 +1,49 @@
+/**
+ * @description 给定两个数组，编写一个函数来计算它们的交集。
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2,2]
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [4,9]
+ */
+
+function calcIntersect(arr1, arr2) {
+    const map1 = makeMap(arr1);
+    const map2 = makeMap(arr2);
+
+    let arr = [];
+    for(let key of map1.keys()) {
+        if(map2.has(key)) {
+            let val1 = map1.get(key);
+            let val2 = map2.get(key); 
+            let minVal = Math.min(val1, val2);
+            for(let i = 0; i < minVal; i++) {
+                arr.push(key);
+            }
+        }
+    }
+
+    return arr;
+}
+
+function makeMap(arr) {
+    let map = new Map();
+    for(let key of arr) {
+        let val = 0;
+        if(map.has(key)) {
+            val = map.get(key);
+        }
+
+        map.set(key, val + 1);
+    }
+    return map;
+}
+
+let arr = calcIntersect([1,2,3,2,1], [3,2,2]);
+
+console.log(arr);
